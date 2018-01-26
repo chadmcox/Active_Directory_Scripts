7564c5601cc0e11afa71654b29a641dc3eb0dae8
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2018-01-26 12:35:41 -0700

    Update CreateADUserCleanUpReports.ps1

--- CreateADUserCleanUpReports.ps1
+++ CreateADUserCleanUpReports.ps1
@@ -40,6 +40,8 @@ from the use or distribution of the Sample Code..
 .EXTERNALSCRIPTDEPENDENCIES 
 
 .RELEASENOTES
+0.14 added search to look for objects with roaming credentials
+    added ability to only through OU's with child objects
 0.12 set functons to global.
 0.11 added switch to import all the functions and not to run the reports.
 0.10 added
@@ -105,8 +107,9 @@ Function global:ADOUList{
         If (!(Test-Path $script:ou_list)){
             Write-host "This will take a few minutes to gather a list of OU's to search through."
             foreach($domain in (get-adforest).domains){
-                try{Get-ADObject -ldapFilter "(|(objectclass=organizationalunit)(objectclass=domainDNS)(objectclass=builtinDomain))" -server $domain | select `
-                     $hash_domain, DistinguishedName  | export-csv $script:ou_list -append -NoTypeInformation}
+                try{Get-ADObject -ldapFilter "(|(objectclass=organizationalunit)(objectclass=domainDNS)(objectclass=builtinDomain))" `
+                    -Properties "msds-approx-immed-subordinates" -server $domain | where {$_."msds-approx-immed-subordinates" -ne 0} | select `
+                     $hash_domain, DistinguishedName | export-csv $script:ou_list -append -NoTypeInformation}
                 catch{"function ADOUList - $domain - $($_.Exception)" | out-file $default_err_log -append}
                 try{(get-addomain $domain).UsersContainer | Get-ADObject -server $domain | select `
                      $hash_domain, DistinguishedName | export-csv $script:ou_list -append -NoTypeInformation}

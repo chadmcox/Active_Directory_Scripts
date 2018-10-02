faef474995e09dc23f5685d6f85737dfc87adcd3
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2018-10-02 10:39:52 -0700

    Update cleanADForeignSecurityPrincipal.ps1

--- cleanADForeignSecurityPrincipal.ps1
+++ cleanADForeignSecurityPrincipal.ps1
@@ -1,7 +1,8 @@
 #Requires -Module ActiveDirectory
+#Requires -version 4.0
 <#PSScriptInfo
 
-.VERSION 0.1
+.VERSION 0.3
 
 .GUID bff8254c-d342-4d67-876e-378d5ca57447
 
@@ -26,6 +27,15 @@ against any claims or lawsuits, including attorneys` fees, that arise or result
 from the use or distribution of the Sample Code..
 
 .RELEASENOTES
+Source: https://github.com/chadmcox/ADPoSh/blob/master/cleanADForeignSecurityPrincipal.ps1
+
+!!!!!!
+-whatif must be removed throughout if wanting to actually perform change, by doing so you acknowledge testing was done.
+!!!!!
+
+0.3 added menus
+    fsp removal only happens if translated object is in the same domain
+    Orphan fsp removal only happens if trust does not exist.
  0.2 fixed to make sure it enumerates each domain
     does a trust domain sid check to make sure trust is still valid
     removes builtin sids from report
@@ -37,7 +47,7 @@ from the use or distribution of the Sample Code..
 Param($reportpath = "$env:userprofile\Documents")
 
 function CollectFSPGroupMembership{
-    
+    write-host "Collecting Groups with ForeignSecurityPrincipals"
     $results = @()
     #enumerate fsp members
 
@@ -49,7 +59,7 @@ function CollectFSPGroupMembership{
     Foreach($domain in (get-adforest).domains){
         #get trust of existing domain
         #$trusted_domain_SIDs = (get-adtrust -filter {intraforest -eq $false} -Properties securityIdentifier -server $domain).securityIdentifier.value
-
+        write-host "Searching $domain"
         Get-ADObject -Filter { objectClass -eq "foreignSecurityPrincipal" } -Properties memberof -server $domain -PipelineVariable fsp | select -ExpandProperty memberof | foreach{
             $group = $_
             if($fsp.Name -match "^S-\d-\d+-\d+-\d+-\d+-\d+"){$domain_sid = $matches[0]}else{$domain_sid = $null}
@@ -63,10 +73,11 @@ function CollectFSPGroupMembership{
     }
     write-host "Found $(($results | measure-object).count) references of FSP in Groups"
     write-host "Found $(($results | where {$_.translate -eq "Orphan"} | measure-object).count) references of Orphan FSP in Groups"
+    write-host "Orphan FSP is usually caused when the sid of the name of the fsp is no longer found in the sidhistory of an object or the object is no longer found over a trust."
+    write-host "Use the report located here: $reportpath and review the fsp's that are orphaned."
     $results | export-csv "$reportpath\reportForeignSecurityPricipalsGroupMembership-$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss')).csv" -NoTypeInformation
     $results
 }
-
 function RemoveFSPFromGroup{
     param($fsp,$adgroupdn,$adgroupdomain,$FSPDomain,$FSPObject)
     if($fspDomain){$fspdomain = $adgroupdomain} #have to set fspdomain of orphan
@@ -92,48 +103,54 @@ function RemoveFSPFromGroup{
                 $status = "$(get-date) - Error!!! Removing $FSP from group $adgroupdn"; write-host $status -ForegroundColor red
                 add-content -value $status -path "$reportpath\logRemoveFSPfromGRP.txt"}
         }else{
-            $status = "$(get-date) - Translated Object not found!! Did Not Remove $FSP from group $adgroupdn"; write-host $status
+            $status = "$(get-date) - Translated Object not found in group!! Did Not Remove $FSP from group $adgroupdn"; write-host $status
             add-content -value $status -path "$reportpath\logRemoveFSPfromGRP.txt"
         }
     }
 }
-
 Function AddTranslatedObjecttoGroup{
     param($FSPDomain,$FSPObject,$adgroupdn,$adgroupdomain)
-    $objtoadd = get-adobject -filter {samaccountname -eq $FSPObject} -server $FSPDomain
 
-    #is object already a member of group
-    if(!(validateTranslatedObjectinGroup -FSPDomain $fspdomain -FSPObjectdn ($objtoadd).distinguishedname `
-        -adgroupdn $adgroupdn -adgroupdomain $adgroupdomain)){
-        #get the group
-        $grp = get-adobject -Identity $adgroupdn -server $adgroupdomain
-        #add the direct object to the group
-        $status = "$(get-date) - Adding $FSPObject to $adgroupdn"; write-host $status
-        add-content -value $status -path "$reportpath\logAddFSPtoGRP.txt"
-        try{Add-ADGroupMember -Identity $grp -Members $objtoadd -whatif}
-            catch{
-            $status = "$(get-date) - Error!!! Adding $FSPObject to $adgroupdn"; write-host $status -ForegroundColor red
+    #to simplify this I made it so that the new object needs to be in the same domain as the group.  
+    #this will make it so that objects still over trust to not readd a new fsp
+    try{$objtoadd = get-adobject -filter {samaccountname -eq $FSPObject} -server $adgroupdomain}
+        catch{$status = "$(get-date) - Error!!!! Object $FSPObject not found in same domain as group: $adgroupdomain"
+            Write-host $status
             add-content -value $status -path "$reportpath\logAddFSPtoGRP.txt"}
-    }else{
-        $status = "$(get-date) - Already Added $FSPObject to $adgroupdn"
-        Write-host $status
-        add-content -value $status -path "$reportpath\logAddFSPtoGRP.txt"
+    if($objtoadd){
+        #is object already a member of group
+        if(!(validateTranslatedObjectinGroup -FSPDomain $fspdomain -FSPObjectdn ($objtoadd).distinguishedname `
+            -adgroupdn $adgroupdn -adgroupdomain $adgroupdomain)){
+            #get the group
+            $grp = get-adobject -Identity $adgroupdn -server $adgroupdomain
+            #add the direct object to the group
+            $status = "$(get-date) - Adding $FSPObject to $adgroupdn"; write-host $status
+            add-content -value $status -path "$reportpath\logAddFSPtoGRP.txt"
+            try{Add-ADGroupMember -Identity $grp -Members $objtoadd -whatif}
+                catch{
+                $status = "$(get-date) - Error!!! Adding $FSPObject to $adgroupdn"; write-host $status -ForegroundColor red
+                add-content -value $status -path "$reportpath\logAddFSPtoGRP.txt"}
+        }else{
+            $status = "$(get-date) - Already Added $FSPObject to $adgroupdn"
+            Write-host $status
+            add-content -value $status -path "$reportpath\logAddFSPtoGRP.txt"
+        }
     }
 }
-
 Function validateTranslatedObjectinGroup{
     param($FSPDomain,$FSPObjectdn,$adgroupdn,$adgroupdomain)
     
-    if(Get-ADgroup -Filter {member -recursivematch $FSPObjectdn} `
+    if(Get-ADgroup -Filter {member -like $FSPObjectdn} `
         -searchbase $adgroupdn -server $adgroupdomain){
         $true
     }else{$false}
 }
-
 function CollectFSPwithNoGroupMembership{
+write-host "Collecting ForeignSecurityPrincipals with no group memberships"
 $results = @()
 #translate Sid
     Foreach($domain in (get-adforest).domains){
+        write-host "Searching $domain"
         $results += Get-ADObject -Filter {objectClass -eq "foreignSecurityPrincipal" -and memberof -notlike "*"} -server $domain | ForEach-Object {$fsp_translate = $null
             $fsp_translate = try{([System.Security.Principal.SecurityIdentifier] $_.Name).Translate([System.Security.Principal.NTAccount])}catch{"Orphan"}
 	        $_ | select $hash_domain,name, `
@@ -143,9 +160,13 @@ $results = @()
     }
     write-host "Found $(($results | measure-object).count) FSP with no Group Membership"
     write-host "Found $(($results |  where {$_.translate -eq "Orphan"} | measure-object).count) Orphan FSP with no Group Membership"
-    $results | export-csv "$reportpath\reportForeignSecurityPricipalsNoGroupMemberShips.csv" -NoTypeInformation
+    write-host "Use the report located here: $reportpath and review the fsp's that are not members of any group, which means they are not used."
+    $results | export-csv "$reportpath\reportForeignSecurityPricipalsNoGroupMemberShips-$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss')).csv" -NoTypeInformation
 }
 function startTranslatedObjectAdds{
+    if(!($allFSP)){
+        $allfsp = CollectFSPGroupMembership
+    }
     #resolvable fsp's actual object will get added to the group.
     $allFSP | where {$_.Translate -ne "Orphan"} | foreach{
         $tranobj = ($_).Translate
@@ -155,6 +176,16 @@ function startTranslatedObjectAdds{
     }
 }
 function startFSPRemovalfromGroups{
+    param([switch]$nottranslated)
+    if(!($allFSP)){
+        $script:allfsp = CollectFSPGroupMembership
+    }
+    if($nottranslated){
+        write-host "Only Removing Orphans from Groups"
+        #this filters the list to objects that do not translate and the trust exist property is null
+        $allfsp = $allfsp | where {$_.translate -eq "Orphan" -and $_.trustexist -eq $null}
+    }
+
     $allfsp | foreach {
         $tranobj = ($_).Translate
         $tranobj = $tranobj -split '\\'
@@ -163,35 +194,89 @@ function startFSPRemovalfromGroups{
 }
 function deleteFSPforever{
     param($reportfile)
+    if(!(Get-ADOptionalFeature -Filter {name -eq "Recycle Bin Feature"} | where {$_.enabledscopes -like "*"})){
+        $continue = read-host "Recycle Bin is not enabled, press y then ENTER to proceed with deletion."
+    }else{
+        $continue = "y"
+    }
+    if($continue -eq "y"){
     import-csv $reportfile | foreach{
         $status = "$(get-date) - Deleting $(($_).name)"; write-host $status
         add-content -value $status -path "$reportpath\logdeleteFSP.txt"
-        try{get-adobject -filter {name -eq $(($_).name)} -server $(($_).domain) | remove-object -whatif}
-            catch{$status = "$(get-date) - Error Deleting $(($_).name)"; write-host $status -ForegroundColor red
+        $fspname = ($_).name
+        $fsddomain = ($_).domain
+        try{get-adobject -filter {name -eq $fspname} -server $fsddomain | remove-adobject -whatif}
+            catch{$_.Exception ;$status = "$(get-date) - Error Deleting $(($_).name)"; write-host $status -ForegroundColor red
                 add-content -value $status -path "$reportpath\logdeleteFSP.txt.txt"}
-    }
+    }}
 }
 function recoverFSPGroupMembership{
     param($reportfile)
+    if($reportfile){
     import-csv $reportfile | foreach{
         get-adobject -Identity $_.Memberof -server $_.Domain | Set-ADObject -add @{'member'="$(($_).distinguishedname)"} -whatif
     }
+    }else{
+        write-host "No file specified"
+    }
+}
+function launchMenu{
+    cls
+    Write-host -ForegroundColor yellow "Select the option you would like to run:"
+    Write-host "   0 - Create Report of all FSP's Group Membership"
+    Write-host "   1 - Create Report of all FSP without Group Membership"
+    Write-host "   2 - Add Valid Translated Object to Group with associated FSP"
+    Write-host "   3 - Remove Orphan FSPs from Groups (only if sid not from existing trusted domain)"
+    Write-host "   4 - Remove ALL FSPs from Groups (validates translated object in group)"
+    Write-host "   5 - Delete FSP from Active Directory (uses option 1 reports)"
+    Write-host "   6 - Restore FSP's Group membership (uses Option 0 reports)"
+    Write-host "   10 - Runs Option 1 & Option 0 to Create Both Reports"
+    
+    
+    $xMenuChoiceA = read-host "Please enter an option 0 to 6 or 10..."
+
+    switch($xMenuChoiceA){
+        0{$script:allFSP = CollectFSPGroupMembership}
+        1{$script:unusedFSP = CollectFSPwithNoGroupMembership}
+        2{startTranslatedObjectAdds}
+        3{startFSPRemovalfromGroups -nottranslated}
+        4{startFSPRemovalfromGroups}
+        5{$removefile = findlist -notamember
+            deleteFSPforever -reportfile $removefile
+        }
+        6{$restorefile = findlist -isamember
+            recoverFSPGroupMembership -reportfile $restorefile}
+        10{CollectFSPwithNoGroupMembership;$script:allFSP = CollectFSPGroupMembership}
+    }
+}
+function findlist{
+    param([switch]$notamember,`
+        [switch]$isamember)
+    $i = 1
+    if($isamember){
+        #this will pull the possible list to remove fsp out of group membership
+        $process = "restore fsp in group membership"
+        $selection = Get-ChildItem -Path "$reportpath\*" -Include reportForeignSecurityPricipalsGroupMembership* | sort LastWriteTime | select -last 5 | foreach{
+            $_ | select fullname, `
+            @{name='Option';expression={$i}}
+            $i++
+        }
+    }elseif($notamember){
+        #this will pull the list of fsp that are best to delete from AD
+        $process = "removal of fsp from AD"
+        $selection = Get-ChildItem -Path "$reportpath\*" -Include reportForeignSecurityPricipalsNoGroupMemberShips* | sort LastWriteTime | select -last 5 | foreach{
+            $_ | select fullname, `
+            @{name='Option';expression={$i}}
+            $i++
+        }
+    }
+    if($selection){
+    cls
+    $selection | Select Option,fullname | out-host
+    $xMenuChoiceA = read-host "Choose the option number of the file you would like to run the $process against?"
+    return ($selection | where {$_.option -eq $xMenuChoiceA}).fullname
+    }
 }
 
-#gather all the groups with FSP's
-$allFSP = CollectFSPGroupMembership
-
-#run this function to put correct object in group
-#startTranslatedObjectAdds
-
-#run this function to remove fsp from groups
-#startFSPRemovalfromGroups
-
-$unusedFSP = CollectFSPwithNoGroupMembership
-
-
-#function for to fsp back into groups
-#recoverFSPGroupMembership -reportfile $reportpath\reportForeignSecurityPricipalsGroupMembership-09-26-2018_04-36-40.csv
-
-#function to remove unused fsp
-#recoverFSPGroupMembership -reportfile $reportpath\reportForeignSecurityPricipalsNoGroupMemberShips.csv
+$allFSP = @()
+launchMenu

83b25e6984c39518f71c2423ce08330efc82aa6c
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2019-03-27 10:09:16 -0700

    Update gatherADTrust.ps1

--- gatherADTrust.ps1
+++ gatherADTrust.ps1
@@ -13,3 +13,19 @@ get-adtrust -filter * -Properties * -server $domain -PipelineVariable trust | se
     @{name='trustAuthIncoming';expression={(Get-ADReplicationAttributeMetadata `
         -filter {attributename -eq "trustAuthIncoming"} -Server (get-addomain $domain).PDCEmulator `
         -Object ($trust).DistinguishedName).LastOriginatingChangeTime}}}
+
+
+<#
+Only show external trust
+#>
+get-adforest | select -expandproperty domains -PipelineVariable domain | foreach{
+get-adtrust -filter * -Properties * -server $domain -PipelineVariable trust | select `
+    @{name='Domain';expression={$domain}},name,securityIdentifier,Created, `
+    Direction,trustType,DisallowTransivity,SelectiveAuthentication,TGTDelegation, `
+    UsesAESKeys,UsesRC4Encryption,whenCreated,whenchanged,`
+    @{name='trustAuthOutgoing';expression={(Get-ADReplicationAttributeMetadata `
+        -filter {attributename -eq "trustAuthOutgoing"} -Server (get-addomain $domain).PDCEmulator `
+        -Object ($trust).DistinguishedName).LastOriginatingChangeTime}}, `
+    @{name='trustAuthIncoming';expression={(Get-ADReplicationAttributeMetadata `
+        -filter {attributename -eq "trustAuthIncoming"} -Server (get-addomain $domain).PDCEmulator `
+        -Object ($trust).DistinguishedName).LastOriginatingChangeTime}}} | where name -notin $((get-adforest).domains)

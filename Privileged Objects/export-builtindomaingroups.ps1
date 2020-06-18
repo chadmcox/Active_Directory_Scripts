e992ee268f7c29bf8f3e34a4a6f96d694fb4b5f7
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-06-18 08:54:56 -0700

    Update export-builtindomaingroups.ps1

--- Privileged Objects/export-builtindomaingroups.ps1
+++ Privileged Objects/export-builtindomaingroups.ps1
@@ -1,3 +1,26 @@
+#Requires -Module ActiveDirectory
+<#PSScriptInfo
+.VERSION 2020.6.18
+.GUID 43c7362f-d300-4bf9-a481-622b67e43137
+.AUTHOR Chad.Cox@microsoft.com
+    https://blogs.technet.microsoft.com/chadcox/
+    https://github.com/chadmcox
+.COMPANYNAME 
+.COPYRIGHT This Sample Code is provided for the purpose of illustration only and is not
+intended to be used in a production environment.  THIS SAMPLE CODE AND ANY
+RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  We grant You a
+nonexclusive, royalty-free right to use and modify the Sample Code and to
+reproduce and distribute the object code form of the Sample Code, provided
+that You agree: (i) to not use Our name, logo, or trademarks to market Your
+software product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is embedded;
+and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and
+against any claims or lawsuits, including attorneys` fees, that arise or result
+from the use or distribution of the Sample Code..
+
+#>
 param($export_file = "$env:userprofile\documents\domain_builtin_group_export.csv")
 #retrieve all of the critical and builtin groups
 $privileged_groups = get-adforest | select -ExpandProperty domains -PipelineVariable domain | foreach{
@@ -13,8 +36,6 @@ $privileged_groups = get-adforest | select -ExpandProperty domains -PipelineVari
 
 #Im only using the Get-ADGroupMember because these groups should have low numbers of members.  wont use this for larger enumeration
 #places a huge strain on domain controller and only returns a subset of members.
-$results = $privileged_groups | foreach{$group = $_; Get-ADGroupMember -Identity $_.distinguishedname -Server $_.domain -Recursive | `
-    select @{n='Domain';e={$group.Domain}},@{n='Group';e={$group.samaccountname}}, samaccountname, displayname, userprincipalname, company, objectclass} | `
-        export-csv $export_file -NoTypeInformation
-
-write-host "Results found here: $export_file"
+$privileged_groups | foreach{$group = $_; Get-ADGroupMember -Identity $_.distinguishedname -Server $_.domain -Recursive | where objectclass -eq "user" | `
+    get-adobject -server "$((get-addomain).PDCEmulator):3268" -property * | select @{n='Domain';e={$group.Domain}},@{n='Group';e={$group.samaccountname}}, `
+        samaccountname, displayname, userprincipalname, company, objectclass} | export-csv $export_file -NoTypeInformation

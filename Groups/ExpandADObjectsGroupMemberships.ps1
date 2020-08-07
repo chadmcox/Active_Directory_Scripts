93c53b55dcc7425a03d0210f0830443ac6954fa5
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-08-07 13:27:44 -0700

    Update ExpandADObjectsGroupMemberships.ps1

--- Groups/ExpandADObjectsGroupMemberships.ps1
+++ Groups/ExpandADObjectsGroupMemberships.ps1
@@ -1,3 +1,29 @@
+#Requires -Module ActiveDirectory
+#Requires -version 4
+<#PSScriptInfo
+.VERSION 2020.7.20
+.GUID 8cc222be-d143-4b38-a8c0-ba25df4e1db1
+.AUTHOR Chad.Cox@microsoft.com
+    https://blogs.technet.microsoft.com/chadcox/
+    https://github.com/chadmcox
+.COMPANYNAME 
+.COPYRIGHT This Sample Code is provided for the purpose of illustration only and is not
+intended to be used in a production environment.  THIS SAMPLE CODE AND ANY
+RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  We grant You a
+nonexclusive, royalty-free right to use and modify the Sample Code and to
+reproduce and distribute the object code form of the Sample Code, provided
+that You agree: (i) to not use Our name, logo, or trademarks to market Your
+software product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is embedded;
+and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and
+against any claims or lawsuits, including attorneys` fees, that arise or result
+from the use or distribution of the Sample Code..
+.description
+this enumerates all of the groups and 
+#>
+
 function expandgpmem{
     [cmdletbinding()]
     param($dn)

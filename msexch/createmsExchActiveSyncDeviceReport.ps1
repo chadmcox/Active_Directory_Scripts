ae051985da607eebd151950ad4504ddc5916c6a5
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2020-11-20 12:14:12 -0700

    Update createmsExchActiveSyncDeviceReport.ps1

--- msexch/createmsExchActiveSyncDeviceReport.ps1
+++ msexch/createmsExchActiveSyncDeviceReport.ps1
@@ -1,3 +1,5 @@
 get-adforest | select -ExpandProperty domains -pv domain | foreach{
-    Get-ADObject -ldapFilter '(objectclass=msExchActiveSyncDevice)' -server $domain | select DistinguishedName, msExch* 
+    get-adobject -ldapfilter "(CN=ExchangeActiveSyncDevices*)" -server $domain | foreach{
+        get-adobject -ldapfilter "(CN=ExchangeActiveSyncDevices*)" -server $domain -searchbase $_.distinguishedname -Properties * | select distinguishedname, msExch*, objectclass
+    }
 } | export-csv .\msExchDevice_export.csv -notypeinformation

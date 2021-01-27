f484ccccaaea56d3f0c9c2ccdb7d8f2bc5492792
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-01-27 15:12:33 -0700

    Update createOUPermreport.ps1

--- Organizational Unit/createOUPermreport.ps1
+++ Organizational Unit/createOUPermreport.ps1
@@ -8,7 +8,8 @@ function getOUPerms{
         Write-host "Gathering OU's from $domain"
         Get-ADObject -ldapFilter "(objectclass=organizationalunit)"  -Properties "msds-approx-immed-subordinates",nTSecurityDescriptor -server $domain | `
             select DistinguishedName -ExpandProperty nTSecurityDescriptor | select distinguishedname -expandproperty access | `
-                where IsInherited -ne $True | select DistinguishedName, IdentityReference, AccessControlType, ActiveDirectoryRights, IsInherited
+                where IsInherited -ne $True | where {!($_.IdentityReference -like "NT AUTHORITY\*") -and !($_.IdentityReference -like "BUILTIN\*") -and !($_.IdentityReference -eq "S-1-5-32-548") -and !($_.IdentityReference -eq "Everyone")} | `
+                    select DistinguishedName, IdentityReference, AccessControlType, ActiveDirectoryRights, IsInherited
     }
 }
 

76907dd31aef8cc7e977dd1f0d67569ce70f0f37
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-03-23 09:13:17 -0700

    Update removeStaleDCSRVRecords.ps1

--- Domain Controller/removeStaleDCSRVRecords.ps1
+++ Domain Controller/removeStaleDCSRVRecords.ps1
@@ -1,1 +1,7 @@
+$dc = Get-ADForest | select -ExpandProperty domains -pv domain | foreach{(Get-ADDomainController -filter * -server $domain).hostname | foreach{"$($_)."}}
+$dc += Get-ADForest | select -ExpandProperty domains -pv domain | foreach{(Get-ADDomainController -filter * -server $domain).IPv4Address}
 
+Get-DnsServerZone | where IsReverseLookupZone -eq $false -pv zone | foreach{
+    Get-DnsServerResourceRecord -ZoneName $zone.ZoneName -RRType Srv | where {$_.RecordData.IPv4Address -notin $dc -and $_.RecordData.NameServer -notin $dc -and $_.RecordData.DomainName -notin $dc} | `
+        remove-DnsServerResourceRecord -ZoneName $zone.ZoneName -WhatIf
+}

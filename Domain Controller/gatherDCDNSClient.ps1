a179677a4651c44ebaa2db719eac67ec9c8757a1
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-03-26 16:42:27 -0700

    Update gatherDCDNSClient.ps1

--- Domain Controller/gatherDCDNSClient.ps1
+++ Domain Controller/gatherDCDNSClient.ps1
@@ -1,4 +1,4 @@
 Get-ADForest | select -ExpandProperty domains -pv domain | foreach{
     Get-ADDomainController -Filter * -Server $domain -pv dc | foreach{
     invoke-command -scriptblock {Get-DnsClientServerAddress | select -ExpandProperty ServerAddresses} -computername $dc.hostname
-}} | select PSComputerName, @{name='DNS';expression={$_}}
+}} | select PSComputerName, @{name='DNS';expression={$_}} | export-csv .\dc_dns_results.csv -notypeinformation

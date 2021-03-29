123ff30f5ddf44a07c4f1341978642696616669d
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-03-29 11:06:10 -0700

    Update checkDFSRelicationState.ps1

--- DFSR/checkDFSRelicationState.ps1
+++ DFSR/checkDFSRelicationState.ps1
@@ -1,6 +1,9 @@
 get-adforest | select -ExpandProperty domains -pv domain | foreach {
     Get-ADDomainController -filter * -server $domain -pv dc | foreach{
+        "--- $($DC.hostname) ----------------------------------"
         write-host "$($DC.hostname)"
-        dfsrdiag.exe ReplicationState /member:"$($DC.hostname)"
+        dfsrdiag.exe ReplicationState /member:"$($DC.hostname)" /all
     }
-}
+} | out-file "$env:TEMP\sysvolreplicationstate.txt"
+
+write-host "Results found here $env:TEMP\sysvolreplicationstate.txt"

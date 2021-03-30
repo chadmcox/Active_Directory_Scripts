5ed69bb74ffe30e4149f2ef3ab30aed799244f88
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-03-30 13:21:58 -0700

    Update getAllDomainACLs.ps1

--- Security/getAllDomainACLs.ps1
+++ Security/getAllDomainACLs.ps1
@@ -1,21 +1,45 @@
-$path = "$env:userprofile\Documents\domain_acls.csv"
+param($reportpath = "$env:userprofile\Documents")
 $schemaIDGUID = @{}
-Get-ADObject -SearchBase (Get-ADRootDSE).schemaNamingContext -LDAPFilter '(schemaIDGUID=*)' -Properties name, schemaIDGUID |
+Get-ADObject -SearchBase (Get-ADRootDSE).schemaNamingContext -LDAPFilter '(schemaIDGUID=*)' -Properties name, schemaIDGUID | `
     ForEach-Object {try{$schemaIDGUID.add([System.GUID]$_.schemaIDGUID,$_.name)}catch{}}
-Get-ADObject -SearchBase "CN=Extended-Rights,$((Get-ADRootDSE).configurationNamingContext)" -LDAPFilter '(objectClass=controlAccessRight)' -Properties name, rightsGUID |
+
+Get-ADObject -SearchBase "CN=Extended-Rights,$((Get-ADRootDSE).configurationNamingContext)" -LDAPFilter '(objectClass=controlAccessRight)' `
+    -Properties name, rightsGUID | `
     ForEach-Object {try{$schemaIDGUID.add([System.GUID]$_.rightsGUID,$_.name)}catch{}}
 
-get-adforest | select -expandproperty domains -pv domain | foreach{
-    try{get-PSDrive -Name ADROOT -ErrorAction SilentlyContinue | Remove-PSDrive -force}catch{}
-    $ps_drive = New-PSDrive -Name ADROOT -PSProvider ActiveDirectory -Server $domain -Scope Global -root "//RootDSE/"
-    $rootdn = "ADROOT:\" + ($domain | get-addomain).DistinguishedName
-    (Get-ACL $rootdn).access | select ObjectType,IdentityReference,ActiveDirectoryRights,`
-        accessControlType -unique | select `
-        @{name='Domain';expression={$domain}}, `
-        @{name='DistinguishedName';expression={$rootdn.Replace("ADROOT:\","")}}, `
-        IdentityReference, `
-        @{name='objectTypeName';expression={if ($_.objectType.ToString() -eq '00000000-0000-0000-0000-000000000000') {'All'} Else {$schemaIDGUID.Item($_.objectType)}}}, `
-        @{name='ActiveDirectoryRights';expression={if($_.ActiveDirectoryRights -ne "ExtendedRight"){$_.ActiveDirectoryRights}else{$schemaIDGUID.Item($_.objectType)}}}, `
-        AccessControlType
-     try{Remove-PSDrive -Name ADROOT -Force}catch{}         
-} | export-csv $path -notypeinformation
+function enumGroup {
+    param($identity,$domain)
+    if(($identity -split "\\")[0] -in "NT AUTHORITY","Everyone"){
+
+    }elseif(($identity -split "\\")[0] -eq "BUILTIN"){
+        try{get-adgroup -Identity ($identity -split "\\")[1] -Properties members -Server $domain | select -ExpandProperty members | foreach{
+            get-adobject -identity $_ -Properties samaccountname -server "$domain`:3268" }}catch{}
+    }else{
+        try{get-adgroup -Identity ($identity -split "\\")[1] -Properties members -Server ($identity -split "\\")[0]| select -ExpandProperty members | foreach{
+            get-adobject -identity $_ -Properties samaccountname -server "$(($identity -split "\\")[0])`:3268" }}catch{}
+    }
+}
+
+function gatherAdminSDHolder {
+    $results = get-adforest | select -ExpandProperty domains -pv domain | foreach {
+        $hash_domain = @{name='Domain';expression={$domain}}
+        Get-ADObject "$((get-addomain -Server $domain).DistinguishedName)" -Properties "msds-approx-immed-subordinates",nTSecurityDescriptor -server $domain -pv container | `
+            select DistinguishedName -ExpandProperty nTSecurityDescriptor | select distinguishedname -expandproperty access -pv perm | foreach{
+                $_ | select $hash_domain,`
+                DistinguishedName, IdentityReference, `
+                AccessControlType, ActiveDirectoryRights, IsInherited, `
+                @{name='objectTypeName';expression={if ($_.objectType.ToString() -eq '00000000-0000-0000-0000-000000000000') {'All'} Else {$schemaIDGUID.Item($_.objectType)}}}, `
+                @{name='inheritedObjectTypeName';expression={$schemaIDGUID.Item($_.inheritedObjectType)}}, @{name='memberofGroup';expression={}}
+    }}
+
+    foreach($obj in $results){
+        $obj
+        enumGroup -identity $obj.IdentityReference -domain $obj.domain | select @{name='Domain';expression={$obj.domain}}, `
+            @{name='DistinguishedName';expression={$obj.DistinguishedName}}, @{name='IdentityReference';expression={if($_.samaccountname){$_.samaccountname}else{$_.name}}}, `
+            @{name='AccessControlType';expression={$obj.AccessControlType}}, @{name='ActiveDirectoryRights';expression={$obj.ActiveDirectoryRights}}, `
+            @{name='IsInherited';expression={$obj.IsInherited}}, @{name='objectTypeName';expression={$obj.objectTypeName}}, `
+            @{name='inheritedObjectTypeName';expression={$obj.inheritedObjectTypeName}},@{name='memberofGroup';expression={$obj.IdentityReference}}
+    }
+}
+
+gatherAdminSDHolder | export-csv $env:userprofile\documents\domain_permissions.csv -NoTypeInformation

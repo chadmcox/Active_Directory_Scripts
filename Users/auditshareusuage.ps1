f4c26efb827169072d34601c8bd379b1af6d1f07
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-10-14 12:04:57 -0700

    Create auditshareusuage.ps1

---
+++ Users/auditshareusuage.ps1
@@ -0,0 +1,6 @@
+get-adforest | select -ExpandProperty domains -pv domain | foreach{
+    Get-ADuser -LDAPFilter "(|(scriptpath=*)(homeDirectory=*))" -Properties scriptpath,homeDirectory -Server $domain | `
+        select @{Name="Domain";Expression={$domain}},SamAccountName,scriptpath,homeDirectory} | `
+            export-csv "$env:USERPROFILE\Documents\mapping.csv" -NoTypeInformation
+            
+write-host results are here "$env:USERPROFILE\Documents\mapping.csv"

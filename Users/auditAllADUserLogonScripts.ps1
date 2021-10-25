592f078b709410a88822b034e0c7acbb452f016b
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2021-10-25 12:53:25 -0700

    Update auditAllADUserLogonScripts.ps1

--- Users/auditAllADUserLogonScripts.ps1
+++ Users/auditAllADUserLogonScripts.ps1
@@ -6,8 +6,8 @@ get-adforest | select -ExpandProperty domains -pv domain | foreach{
             
 get-adforest | select -ExpandProperty domains -pv domain | foreach{
     Get-ADuser -LDAPFilter "(scriptpath=*)" -Properties scriptpath,homeDirectory -Server $domain | foreach{ 
-        $file = $null;if($_.scriptpath -like "\\*"){$file = get-item $_.scriptpath}
-            else{$file = get-item "\\$domain\NETLOGON\$($_.scriptpath)" -ErrorAction SilentlyContinue}
+        $file = $null;if($_.scriptpath  -like "\\*"){$file = get-item ($_.scriptpath -split " ")[0] -ErrorAction SilentlyContinue}
+            else{$file = get-item "\\$domain\NETLOGON\$(($_.scriptpath -split " ")[0])" -ErrorAction SilentlyContinue}
         $_ | select @{Name="Domain";Expression={$domain}},SamAccountName,scriptpath,@{Name="LastModified";Expression={$file.LastWriteTimeUtc}},
             @{Name="LastAccessed";Expression={$file.LastAccessTimeUtc}}}} | `
                 export-csv "$env:USERPROFILE\Documents\scriptDates.csv" -NoTypeInformation

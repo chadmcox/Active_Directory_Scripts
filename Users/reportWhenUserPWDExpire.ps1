595f11a16e9f5546c6425b028a20476864d53a3b
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-08-10 10:05:02 -0700

    Update reportWhenUserPWDExpire.ps1

--- Users/reportWhenUserPWDExpire.ps1
+++ Users/reportWhenUserPWDExpire.ps1
@@ -1,5 +1,6 @@
 get-adforest | select -ExpandProperty domains -pv domain | foreach{
     Get-ADUser -filter * –Properties DisplayName,PasswordLastSet,PasswordExpired, "msDS-UserPasswordExpiryTimeComputed" -Server $domain | `
     Select-Object -Property @{Name="Domain";Expression={$domain}},samaccountname,PasswordExpired,PasswordLastSet,`
+        @{Name="pwdLastSet";Expression={if($_.PwdLastSet -ne 0){([datetime]::FromFileTime($_.pwdLastSet).ToString('MM/dd/yyyy'))}}},
         @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}
 } | export-csv .\ad_user_when_password_change.csv -NoTypeInformation
